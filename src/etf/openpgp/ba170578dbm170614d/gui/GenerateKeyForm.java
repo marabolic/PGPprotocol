f11b24b2c104733c1e459fe07f71500d92148f53
Aleksa Bogdanović <51212867+aleksa1902@users.noreply.github.com>
2021-05-27 20:04:20 +0200

    GUI_prototype v0.1

--- src/etf/openpgp/ba170578dbm170614d/gui/GenerateKeyForm.java
+++ src/etf/openpgp/ba170578dbm170614d/gui/GenerateKeyForm.java
@@ -1,24 +1,49 @@
 package etf.openpgp.ba170578dbm170614d.gui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ItemListener;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class GenerateKeyForm {
     private JTextField nameTextField;
     private JButton submitButton;
     private JTextField emailTextField;
     private JComboBox asymetricAlgorithms;
+    private JTable KeyTable;
+    private JButton deleteKeyButton;
+    private JPanel GenerateKeyFormPanel;
 
+    private static final String regex = "^(.+)@(.+)$";
 
+    boolean ValidEmail(String email){
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(email);
+        if(matcher.matches()) return true;
+        else return false;
+    }
 
     void initComponents(){
-        String [] algrorithms = {"DSA", "El Gamal"};
+        String [] algrorithms = {"DSA 1024", "DSA 2048", "El Gamal 1024", "El Gamal 2048", "El Gamal 4096"};
         asymetricAlgorithms = new JComboBox(algrorithms);
 
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(ValidEmail(emailTextField.getText()) && nameTextField.getText() != null){
+                    new PasswordForm();
+                }
+            }
+        });
     }
 
     GenerateKeyForm() {
         initComponents();
+
+
     }
 
 
---
+++ src/etf/openpgp/ba170578dbm170614d/gui/RecieveMessageForm.java
@@ -0,0 +1,11 @@
+package etf.openpgp.ba170578dbm170614d.gui;
+
+import javax.swing.*;
+
+public class RecieveMessageForm {
+    private JTextField BrowseTextField;
+    private JButton BrowseButton;
+    private JLabel RecieveMessageLabel;
+    private JButton decryptionButton;
+    private JPanel RecieveMessageFormPanel;
+}
