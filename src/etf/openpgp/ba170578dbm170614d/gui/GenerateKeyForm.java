d2260bed1c07f0095516089bf03c4cb7e7954e7e
Aleksa Bogdanović <51212867+aleksa1902@users.noreply.github.com>
2021-06-10 23:15:44 +0200

    DeleteKey

--- src/etf/openpgp/ba170578dbm170614d/gui/GenerateKeyForm.java
+++ src/etf/openpgp/ba170578dbm170614d/gui/GenerateKeyForm.java
@@ -1,10 +1,19 @@
 package etf.openpgp.ba170578dbm170614d.gui;
 
 import etf.openpgp.ba170578dbm170614d.pgp.GenerateKeys;
+import org.bouncycastle.openpgp.PGPException;
+import org.bouncycastle.openpgp.PGPPublicKeyRing;
+import org.bouncycastle.openpgp.PGPSecretKeyRing;
+import org.bouncycastle.openpgp.operator.PBESecretKeyDecryptor;
+import org.bouncycastle.openpgp.operator.bc.BcPBESecretKeyDecryptorBuilder;
+import org.bouncycastle.openpgp.operator.bc.BcPGPDigestCalculatorProvider;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Iterator;
+import java.util.Vector;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,10 +22,13 @@ public class GenerateKeyForm {
     private JButton submitButton;
     private JTextField emailTextField;
     private JComboBox asymetricAlgorithms;
-    private JTable KeyTable;
     private JButton deleteKeyButton;
     private JPanel generateKeyFormPanel;
     private JButton BackButton;
+    private JComboBox publicKeyComboBox;
+    private JComboBox secretKeyComboBox;
+    private JLabel PublicKey;
+    private JLabel secretKey;
     private JFrame frame;
 
     private static final String regex = "^(.+)@(.+)$";
@@ -40,18 +52,34 @@ public class GenerateKeyForm {
         asymetricAlgorithms.addItem("ElGamal 4096");
 
         asymetricAlgorithms.setSelectedIndex(0);
+        overviewKey();
 
         frame.setVisible(true);
-
-
 
         submitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if(ValidEmail(emailTextField.getText()) && nameTextField.getText() != null){
-                    PasswordForm pf= new PasswordForm();
+                    //PasswordForm pf= new PasswordForm(frame);
+                    String password = JOptionPane.showInputDialog(frame,"Password");
                     GenerateKeys generateKeys = new GenerateKeys();
-                    generateKeys.generateKeys();
+                    int selectedIndex = asymetricAlgorithms.getSelectedIndex();
+                    int elgParam = 1024;
+                    switch (selectedIndex){
+                        case 0: elgParam = 1024; break;
+                        case 1: elgParam = 2048; break;
+                        case 2: elgParam = 4096;
+                    }
+
+                    System.out.println("email: " + emailTextField.getText());
+                    System.out.println("pass: " + password);
+                    generateKeys.generateKeys(1024, elgParam, emailTextField.getText(), password.toCharArray());
+
+                    overviewKey();
+                    publicKeyComboBox.revalidate();
+                    secretKeyComboBox.revalidate();
+                    publicKeyComboBox.repaint();
+                    secretKeyComboBox.repaint();
                 }else{
                     JOptionPane.showMessageDialog(frame, "Something wrong, check your email or name.");
                 }
@@ -65,10 +93,108 @@ public class GenerateKeyForm {
                 MainFrame.setVisible(true);
             }
         });
+
+        deleteKeyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String[] publicKey = publicKeyComboBox.getSelectedItem().toString().split(" ");
+                String[] secretKey = secretKeyComboBox.getSelectedItem().toString().split(" ");
+
+                System.out.println(publicKey[1]);
+                System.out.println(secretKey[1]);
+
+                if(publicKey[1].equals(secretKey[1])){
+                    String password = JOptionPane.showInputDialog(frame,"Password");
+                    deleteKeys(secretKey[1], password);
+
+                    overviewKey();
+                    publicKeyComboBox.revalidate();
+                    secretKeyComboBox.revalidate();
+                    publicKeyComboBox.repaint();
+                    secretKeyComboBox.repaint();
+                }else{
+                    JOptionPane.showMessageDialog(frame, "Something wrong, private and secret key id isnt equal.");
+                }
+            }
+        });
     }
 
     GenerateKeyForm(JFrame frame) {
         initComponents(frame);
+    }
+
+    private void overviewKey(){
+        publicKeyComboBox.removeAll();
+        secretKeyComboBox.removeAll();
+        System.out.println("USAO SAM U OVER");
+
+        if(GenerateKeys.pgpPublicKeyRing != null){
+            for (Iterator<PGPPublicKeyRing> iterator = GenerateKeys.pgpPublicKeyRing.iterator(); iterator.hasNext();) {
+                PGPPublicKeyRing next = iterator.next();
+
+                String email = "";
+                Iterator<?> mail = next.getPublicKey().getUserIDs();
+
+                while (mail.hasNext()) {
+                    email += (String) mail.next();
+                }
+
+                publicKeyComboBox.addItem(email + " " + next.getPublicKey().getKeyID());
+                System.out.println("Proso public");
+            }
+
+            for (Iterator<PGPSecretKeyRing> iterator = GenerateKeys.pgpSecretKeyRing.iterator(); iterator.hasNext();) {
+                PGPSecretKeyRing next = iterator.next();
+
+                String email = "";
+                Iterator<?> mail = next.getPublicKey().getUserIDs();
+
+                while (mail.hasNext()) {
+                    email += (String) mail.next();
+                }
+
+                secretKeyComboBox.addItem(email + " " + next.getSecretKey().getKeyID());
+                System.out.println("Proso secret");
+            }
+        }
+
+    }
+
+    private void deleteKeys(String keyID,String password){
+        PGPSecretKeyRing next = null;
+        for (Iterator<PGPSecretKeyRing> iterator = GenerateKeys.pgpSecretKeyRing.iterator(); iterator.hasNext();) {
+            next = iterator.next();
+            if ((next.getSecretKey().getKeyID() + "").equals(keyID)) {
+                break;
+            }
+        }
+
+        PBESecretKeyDecryptor dec = new BcPBESecretKeyDecryptorBuilder(new BcPGPDigestCalculatorProvider()).build(password.toCharArray());
+
+        try {
+            if(next.getSecretKey().extractPrivateKey(dec) != null){
+                System.out.println("SIFRA DOBRA");
+                System.out.println("PRIVATNI OBRISAN");
+                GenerateKeys.pgpSecretKeyRing.remove(next);
+
+                PGPPublicKeyRing nextPublic = null;
+                for (Iterator<PGPPublicKeyRing> iterator = GenerateKeys.pgpPublicKeyRing.iterator(); iterator.hasNext();) {
+                    nextPublic = iterator.next();
+                    if ((nextPublic.getPublicKey().getKeyID() + "").equals(keyID)) {
+                        break;
+                    }
+
+                }
+
+                GenerateKeys.pgpPublicKeyRing.remove(nextPublic);
+                System.out.println("JAVNI OBRISAN");
+            }else{
+                System.out.println("SIFRA NIJE DOBRA");
+            }
+
+        } catch (PGPException e) {
+            e.printStackTrace();
+        }
     }
 
 }
